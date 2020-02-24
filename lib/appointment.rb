c26368fe2474b3d1645372977195086ed3de3044
Christopher Leja <christopher.leja@gmail.com>
2020-02-24 14:33:05 +0000

    Automatically backed up by Learn

---
+++ lib/appointment.rb
@@ -0,0 +1,18 @@
+class Appointment
+  
+  attr_accessor :patient, :date, :doctor
+  
+  @@all = []
+  
+  def initialize(patient, date, doctor)
+    @date = date
+    @doctor = doctor
+    @patient = patient
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+end
