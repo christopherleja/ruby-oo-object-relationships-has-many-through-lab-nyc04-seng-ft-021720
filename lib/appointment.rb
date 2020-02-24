c9489538a71a72a5b5a5583e6d5d2d4aed927adb
Christopher Leja <christopher.leja@gmail.com>
2020-02-24 14:35:43 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -4,7 +4,7 @@ class Appointment
   
   @@all = []
   
-  def initialize(patient, date, doctor)
+  def initialize(date, doctor, patient)
     @date = date
     @doctor = doctor
     @patient = patient
